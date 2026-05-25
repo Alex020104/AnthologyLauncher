29149bc838add11910055cd50d2742afc395a3ac
sysliveprime-ctrl <sysliveprime-ctrl@users.noreply.github.com>
2026-05-25 05:12:16 +0300

    Add self update restart diagnostics

--- anthology_launcher.py
+++ anthology_launcher.py
@@ -31,7 +31,7 @@ RENDER_LABELS = {
     "DX8": "DirectX 8 / R0",
 }
 SHADOWS = [1536, 2048, 2560, 3072, 4096]
-LAUNCHER_VERSION = "2026.05.25.4"
+LAUNCHER_VERSION = "2026.05.25.5"
 LAUNCHER_VERSION_URL = "https://api.github.com/repos/sysliveprime-ctrl/AnthologyLauncher/contents/launcher_version.json?ref=main"
 LAUNCHER_VERSION_RAW_URL = "https://raw.githubusercontent.com/sysliveprime-ctrl/AnthologyLauncher/main/launcher_version.json"
 LAUNCHER_EXE_URL = "https://github.com/sysliveprime-ctrl/AnthologyLauncher/releases/latest/download/AnomalyLauncher.exe"
@@ -1314,13 +1314,22 @@ class LauncherApp(tk.Tk):
             "if exist \"%DST%\" for %%A in (\"%DST%\") do echo dst_size_after=%%~zA >> \"%LOG%\"",
             "echo launcher updated; restarting >> \"%LOG%\"",
             "timeout /t 2 /nobreak >nul",
+            "echo env before cleanup: >> \"%LOG%\"",
+            "set _PYI >> \"%LOG%\" 2>&1",
+            "set _MEI >> \"%LOG%\" 2>&1",
             "echo clearing PyInstaller environment >> \"%LOG%\"",
             "for /f \"tokens=1 delims==\" %%E in ('set _PYI 2^>nul') do set \"%%E=\"",
             "for /f \"tokens=1 delims==\" %%E in ('set _MEI 2^>nul') do set \"%%E=\"",
             "set \"PYINSTALLER_RESET_ENVIRONMENT=1\"",
+            "echo env after cleanup: >> \"%LOG%\"",
+            "set _PYI >> \"%LOG%\" 2>&1",
+            "set _MEI >> \"%LOG%\" 2>&1",
             "echo PYINSTALLER_RESET_ENVIRONMENT=%PYINSTALLER_RESET_ENVIRONMENT% >> \"%LOG%\"",
+            "echo start command: start \"\" /D \"%DST_DIR%\" \"%DST%\" >> \"%LOG%\"",
             "start \"\" /D \"%DST_DIR%\" \"%DST%\"",
             "echo restart requested >> \"%LOG%\"",
+            "timeout /t 1 /nobreak >nul",
+            "tasklist /FI \"IMAGENAME eq AnomalyLauncher.exe\" >> \"%LOG%\" 2>&1",
             "timeout /t 3 /nobreak >nul",
             "del /q \"%SRC%\" >> \"%LOG%\" 2>&1",
             "del /q \"%~f0\" >nul 2>&1",
