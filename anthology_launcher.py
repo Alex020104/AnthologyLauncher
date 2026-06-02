2596cfaae89fc8046723ad66785926b3aa03f989
sysliveprime-ctrl <sysliveprime-ctrl@users.noreply.github.com>
2026-06-02 12:39:35 +0300

    Bump launcher to 2026.06.02.2

--- anthology_launcher.py
+++ anthology_launcher.py
@@ -31,7 +31,7 @@ RENDER_LABELS = {
     "DX8": "DirectX 8 / R0",
 }
 SHADOWS = [1536, 2048, 2560, 3072, 4096]
-LAUNCHER_VERSION = "2026.06.02.1"
+LAUNCHER_VERSION = "2026.06.02.2"
 LAUNCHER_VERSION_URL = "https://api.github.com/repos/sysliveprime-ctrl/AnthologyLauncher/contents/launcher_version.json?ref=main"
 LAUNCHER_VERSION_RAW_URL = "https://raw.githubusercontent.com/sysliveprime-ctrl/AnthologyLauncher/main/launcher_version.json"
 LAUNCHER_EXE_URL = "https://github.com/sysliveprime-ctrl/AnthologyLauncher/releases/latest/download/AnomalyLauncher.exe"
@@ -55,6 +55,9 @@ DB_REPO = "https://github.com/sysliveprime-ctrl/anthology-db"
 DB_UPDATE_VERSION_URL = "https://api.github.com/repos/sysliveprime-ctrl/anthology-db/contents/db_version.json?ref=main"
 DB_UPDATE_VERSION_RAW_URL = "https://raw.githubusercontent.com/sysliveprime-ctrl/anthology-db/main/db_version.json"
 DB_ALLOWED_PARTS = {"configs", "mods"}
+DB_PRESERVE_PATHS = {
+    "db/mods/00_modded_exes_gamedata.db0",
+}
 
 COLORS = {
     "bg": "#050807",
@@ -2061,7 +2064,7 @@ class LauncherApp(tk.Tk):
                 if not path.is_file() or not self._is_db_archive_file(path):
                     continue
                 rel = path.relative_to(self.root_dir).as_posix().casefold()
-                if rel in allowed:
+                if rel in allowed or rel in DB_PRESERVE_PATHS:
                     continue
                 self._make_writable(path)
                 path.unlink()
